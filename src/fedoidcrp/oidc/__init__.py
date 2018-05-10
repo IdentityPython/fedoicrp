dabfb4a90301b8f7b627d8307af8ef48cd88af05
roland <roland@catalogix.se>
2018-05-10 16:46:15 +0200

    Updated version

--- src/fedoidcrp/oidc/__init__.py
+++ src/fedoidcrp/oidc/__init__.py
@@ -7,11 +7,11 @@ class RP(oidc.RP):
                  keyjar=None, verify_ssl=True, config=None, client_cert=None,
                  httplib=None, services=None, service_factory=None):
         oidc.RP.__init__(self, state_db, ca_certs,
-                               client_authn_factory=client_authn_factory,
-                               keyjar=keyjar, verify_ssl=verify_ssl,
-                               config=config, client_cert=client_cert,
-                               httplib=httplib, services=services,
-                               service_factory=service_factory)
+                         client_authn_factory=client_authn_factory,
+                         keyjar=keyjar, verify_ssl=verify_ssl,
+                         config=config, client_cert=client_cert,
+                         httplib=httplib, services=services,
+                         service_factory=service_factory)
 
         fe = make_federation_entity(config['federation'], '', self.http)
         self.service_context.federation_entity = fe
